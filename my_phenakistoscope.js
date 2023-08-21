eb379e3c6fb946d1378d3be1099b986ad2a3be20
IrohKnowsBest <alysjeffries7@hotmail.com>
2023-08-21 14:30:03 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,9 +1,9 @@
 const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
@@ -12,7 +12,7 @@ function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces,);
+  var layer1 = new PLayer();
   layer1.mode( SWIRL(5) );
   layer1.set_boundary(0,1000);
 
@@ -30,6 +30,13 @@ function setup_layers(pScope){
 }
 
 function sun(x,y,animation,pScope){
+  let angleOffset = (360 / SLICE_COUNT) / 2
+  let backgroundArcStart = 270 - angleOffset;
+  let backgroundArcEnd = 270 + angleOffset;
+
+  fill(70, 80, 108);
+  arc(x,y,400,400,backgroundArcStart,backgroundArcEnd);
+
   rotate(animation.wave());
   fill(244, 128, 55);
   noStroke();
@@ -83,4 +90,12 @@ function squares(x, y, animation, pScope){
   fill(255)
   rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
 
+}
+function planet1(){
+  let angleOffset = (360 / SLICE_COUNT) / 2
+  let backgroundArcStart = 270 - angleOffset;
+  let backgroundArcEnd = 270 + angleOffset;
+
+  fill(37,42,54)
+  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd);
 }
